8b7d679466ed5602a5703766ad3e57003007ff22
MUSAYEV07 <musayevotabek083@gmail.com>
2026-03-11 00:38:35 +0500

    CountUp ham qoshdim

--- Desktop/Abutech/src/components/Statistics/Statistic.jsx
+++ Desktop/Abutech/src/components/Statistics/Statistic.jsx
@@ -10,30 +10,36 @@ const Statistic = () => {
           <div className="statistic-card">
             <div className="count">
               <CountUp start={0} end={40} duration={3} separator="," />
+              <span>+</span>
             </div>
-            <span>+</span>
+            
+            <p className="malumot">Completed projects</p>
           </div>
 
           <div className="statistic-card">
             <div className="count">
               <CountUp start={0} end={10} duration={3} separator="," />
+              <span>+</span>
             </div>
-            <span>+</span>
+            <p className="malumot">Happy clients</p>
           </div>
 
           <div className="statistic-card">
             <div className="count">
               <CountUp start={0} end={30} duration={3} separator="," />
+              <span>+</span>
             </div>
-            <span>+</span>
+            <p className="malumot">Skilled professionals</p>
           </div>
 
           <div className="statistic-card">
             <div className="count">
               <CountUp start={0} end={1} duration={3} separator="," />
+              <span>+</span>
             </div>
-            <span>+</span>
+            <p className="malumot">Awards won</p>
           </div>
+          
         </div>
       </div>
     </section>
